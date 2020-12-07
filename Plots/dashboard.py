dd464b003fd3a354a6f3846862a4537ebfa5cbd8
Jamie Stephens <jsteph61@uncc.edu>
2020-12-07 17:37:17 -0500

    Added line chart with dropdown for country selection

--- Plots/dashboard.py
+++ Plots/dashboard.py
@@ -32,9 +32,9 @@ df_country.drop(df_country.iloc[:, 2:35], inplace = True, axis = 1)  #temporary 
 df_country.dropna
 df_country = df_country.rename(columns={'ISO.Code': 'ISOCode', 'Country.Name': 'CountryName'})
 df_country.drop(df_country.tail(1).index,inplace=True)
+df_country1 = df_country.copy(deep=False)
 df_country = pd.melt(df_country,id_vars=['ISOCode','CountryName'], var_name="Year", value_name="Deaths")
 #======================================================================================
-
 
 #======= Altering the df_country dataframe ============================================
 df_totalpop.drop(['Index','Variant','Notes','Country code','Parent code','1950','1951','1952','1953','1954','2020'], axis='columns', inplace=True)
@@ -47,18 +47,20 @@ df_totalpop = df_totalpop[df_totalpop.Type != 'Development Group']
 df_totalpop = df_totalpop[df_totalpop.Type != 'Special other']
 df_totalpop = df_totalpop[df_totalpop.Type != 'SDG region']
 df_totalpop.drop(['Type'], axis='columns', inplace=True)
-df_totalpop = df_totalpop.rename(columns={'Region, subregion, country or area *': 'Country'})
-df_totalpop = pd.melt(df_totalpop,id_vars=['Country'],var_name="Year",value_name="Population")
+df_totalpop = df_totalpop.rename(columns={'Region, subregion, country or area *': 'CountryName'})
+df_totalpop = pd.melt(df_totalpop,id_vars=['CountryName'],var_name="Year",value_name="Population")
 #======================================================================================
 
 
 # to do: create merged dataframe: country,year,child deaths, total pop, child deaths per capita
 # print("Total population:")
+# print(df_totalpop.keys())
 # print(df_totalpop)
 # print("Country deaths:")
+# print(df_country.keys())
 # print(df_country)
-# df_map_merged = pd.merge(df_country,df_totalpop,how="inner",on=['Country','Year'])
-# print(df_map_merged)
+# df_country.merge(df_totalpop,on='CountryName')
+# print(df_country)
 
 fig = px.choropleth(data_frame = df_country,
                     locations= "ISOCode",
@@ -66,6 +68,47 @@ fig = px.choropleth(data_frame = df_country,
                     hover_name= "CountryName",
                     color_continuous_scale= 'RdYlGn_r',
                     animation_frame= "Year")
+
+#=======Interactive line chart ======================================================
+df_country1 = df_country1.set_index(['CountryName'])
+df_country1.drop(['ISOCode'], axis='columns', inplace=True)
+df_country1 = df_country1.T
+df_country1.iloc[0] = 0
+
+fig1 = go.Figure()
+
+# set up ONE trace
+fig1.add_trace(go.Scatter(x=df_country1.index,
+                         y=df_country1[df_country1.columns[0]],
+                         visible=True)
+             )
+
+updatemenu = []
+buttons = []
+# button with one option for each dataframe
+for col in df_country1.columns:
+    buttons.append(dict(method='restyle',
+                        label=col,
+                        visible=True,
+                        args=[{'y':[df_country1[col]],
+                               'x':[df_country1.index],
+                               'type':'scatter'}, [0]],
+                        )
+                  )
+
+# some adjustments to the updatemenus
+updatemenu = []
+your_menu = dict()
+updatemenu.append(your_menu)
+
+updatemenu[0]['buttons'] = buttons
+updatemenu[0]['direction'] = 'down'
+updatemenu[0]['showactive'] = True
+
+# add dropdown menus to the figure
+fig1.update_layout(showlegend=False, updatemenus=updatemenu)
+fig1.show()
+#==============================================================================
 
 
 # Layout
@@ -82,6 +125,9 @@ app.layout = html.Div(children=[
     html.Div('This chloropleth map shows national mortality rates over time.'),
 
     dcc.Graph(figure=fig),
+    html.H3('Line Graph', style={'color': '#df1e56'}),
+    html.Div('Same data, with a dropdown.'),
+    dcc.Graph(figure=fig1),
     ])
 
 if __name__ == '__main__':
